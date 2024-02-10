1be2e22951416c8c826c38ca775e11b475b52be4
jalvarez-2021317 <jalvarez-2021317@kinal.edu.gt>
2024-02-10 08:32:17 -0600

    Get Emplooye by Id

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,22 +1,38 @@
-import {pool} from "../db.js"
+import { pool } from "../db.js"
 
-export const getEmployees = async (req,res) => {
-const [rows] = await pool.query("SELECT * FROM employees")
-res.json(rows)
+const getEmployees = async (req, res) => {
+    const [rows] = await pool.query("SELECT * FROM employees")
+    res.json(rows)
 }
 
-export const createEmployees = async (req, res) => {
-    const {name,salary}= req.body
-const [rows]= await pool.query("INSERT INTO employees (name, salary) VALUES (?,?)", [name ,salary])
-res.send({
-    id: rows.insertId,
-    name,
-    salary
+const getEmployee = async (req, res) => {
+ const [rows] = await pool.query("SELECT  * FROM employees WHERE id = ?",[req.params.id])
+if (rows.length<= 0) return res.status(404).json({
+    message:"Employee not found"
 })
+
+res.json(rows[0])
+
 }
 
-export const updateEmployees =  (req, res) => res.send("Obteniendo empleados")
+const createEmployees = async (req, res) => {
+    const { name, salary } = req.body
+    const [rows] = await pool.query("INSERT INTO employees (name, salary) VALUES (?,?)", [name, salary])
+    res.send({
+        id: rows.insertId,
+        name,
+        salary
+    })
+}
 
-export const deleteEmployees = (req, res) => res.send("Obteniendo empleados")
+const updateEmployees = (req, res) => res.send("Obteniendo empleados")
+
+const deleteEmployees = (req, res) => res.send("Obteniendo empleados")
 
 
+export {
+    getEmployee,
+    getEmployees,
+    createEmployees,
+    updateEmployees, deleteEmployees
+}
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { createEmployees, deleteEmployees, updateEmployees, getEmployees } from "../controllers/employees.controllers.js"
+import { createEmployees, deleteEmployees, updateEmployees, getEmployees, getEmployee } from "../controllers/employees.controllers.js"
 
 const router = Router()
 
-router.get("/employes", getEmployees)
+router.get("/employees", getEmployees)
+router.get("/employee/:id",getEmployee)
 router.post("/employes", createEmployees)
 router.put("/employes",updateEmployees)
 router.delete("/employes",deleteEmployees )
